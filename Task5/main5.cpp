1122c10f1d6720049b29c4e051bb3b97c6063e55
LoliYori <148603196+LoliYori@users.noreply.github.com>
2025-06-27 12:23:36 +0500

    Update main5.cpp

--- Task5/main5.cpp
+++ Task5/main5.cpp
@@ -1,32 +1,26 @@
 #include "container_operations.h"
-#include <iostream>
 
 int main() {
     try {
         std::deque<int> dequeContainer;
         std::list<int> listContainer;
-        int containerSize;
+        int containerSize = 0;
 
-        // Fill deque
         std::cout << "Enter deque size (>=5): ";
         std::cin >> containerSize;
         FillContainer(dequeContainer, containerSize);
 
-        // Fill list
         std::cout << "Enter list size (>=5): ";
         std::cin >> containerSize;
         FillContainer(listContainer, containerSize);
 
-        // Print original containers
         std::cout << "\nOriginal deque:\n";
         PrintContainer(dequeContainer);
         std::cout << "Original list:\n";
         PrintContainer(listContainer);
 
-        // Insert elements
         InsertElements(dequeContainer, listContainer);
 
-        // Print results
         std::cout << "\nAfter insertion:\n";
         std::cout << "Deque:\n";
         PrintContainer(dequeContainer);
