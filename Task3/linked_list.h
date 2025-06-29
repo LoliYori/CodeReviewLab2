8ea545277d7ebc4ddfbf16a612257eac63857e1c
LoliYori <148603196+LoliYori@users.noreply.github.com>
2025-06-30 00:13:04 +0500

    Update linked_list.h

--- Task3/linked_list.h
+++ Task3/linked_list.h
@@ -3,59 +3,24 @@
 
 #include <iostream>
 
-/**
- * @brief Узел односвязного списка
- */
 struct Node {
     int data;     
     Node* next; 
 };
 
-/**
- * @brief Класс для работы с односвязным списком
- */
 class LinkedList {
-private:
-    Node* head;     
-    Node* tail;  
-
 public:
     LinkedList();
     ~LinkedList();
-
-    /**
-     * @brief Добавляет элемент в конец списка
-     * @param value Значение для добавления
-     */
     void Append(int value);
-
-    /**
-     * @brief Вставляет значение после каждого второго элемента
-     * @param value Значение для вставки
-     */
     void InsertAfterEverySecond(int value);
-
-    /**
-     * @brief Выводит список
-     */
     void Print() const;
-
-    /**
-     * @brief Возвращает указатель на последний элемент
-     * @return Указатель на последний узел
-     */
     Node* GetLastNode() const;
-
-    /**
-     * @brief Удаляет элемент по значению
-     * @param value Значение для удаления
-     */
     void Remove(int value);
 
 private:
-    /**
-     * @brief Очищает список
-     */
+    Node* head;     
+    Node* tail;  
     void Clear();
 };
 
