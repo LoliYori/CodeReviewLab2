8b55d1fbbc095139334e95286ad412d10802b423
LoliYori <148603196+LoliYori@users.noreply.github.com>
2025-06-30 00:09:54 +0500

    Update queue.h

--- Task2/queue.h
+++ Task2/queue.h
@@ -3,9 +3,6 @@
 
 #include <iostream>
 
-/**
- * @brief Узел очереди
- */
 struct Node {
     int data;     
     Node* next;  
@@ -13,50 +10,19 @@ struct Node {
     Node(int val) : data(val), next(nullptr) {}
 };
 
-/**
- * @brief Класс для работы с очередью
- */
 class Queue {
-private:
-    Node* head;   
-    Node* tail;  
-
 public:
     Queue();
     ~Queue();
-
-    /**
-     * @brief Добавляет элемент в конец очереди
-     * @param value Значение для добавления
-     */
     void Enqueue(int value);
-
-    /**
-     * @brief Извлекает N элементов из начала очереди
-     * @param count Количество элементов для извлечения
-     */
     void ExtractElements(int count);
-
-    /**
-     * @brief Выводит исходное состояние очереди
-     */
     void PrintInitialState() const;
-
-    /**
-     * @brief Выводит состояние очереди после извлечения элементов
-     */
     void PrintFinalState() const;
-
-    /**
-     * @brief Проверяет, пуста ли очередь
-     * @return true если очередь пуста, false в противном случае
-     */
     bool IsEmpty() const;
 
 private:
-    /**
-     * @brief Очищает очередь
-     */
+    Node* head;   
+    Node* tail;  
     void Clear();
 };
 
