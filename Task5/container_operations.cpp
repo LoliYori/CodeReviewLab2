220b418456e6253bf332ae223b87e07fc1dfa94d
LoliYori <148603196+LoliYori@users.noreply.github.com>
2025-06-30 00:26:19 +0500

    Update container_operations.cpp

--- Task5/container_operations.cpp
+++ Task5/container_operations.cpp
@@ -1,26 +1,26 @@
 #include "container_operations.h"
 
 template <typename Container>
-void FillContainer(Container& container, int size) {
+bool ContainerOperations::FillContainer(Container& container, int size) {
     if (size < 5) {
-        std::cerr << "Îøèáêà: Ðàçìåð êîíòåéíåðà äîëæåí áûòü íå ìåíåå 5\n";
+        std::cerr << "Ошибка: Размер контейнера должен быть не менее 5\n";
         return false;
     }
 
     int method = 0;
-    std::cout << "Âûáåðåòå ñïîñîá çàïîëíåíèÿ êîíòåéíåðà: \n"
-        << "1)Ââîä ñ êëàâèàòóðû.\n"
-        << "2)Ââîä ñ ïîìîùüþ ðàíäîìàéçåðà.\n"
-        << "3)Ââîä äàííûõ èç òåêñòîâîãî ôàéëà.\n";
+    std::cout << "Выберите способ заполнения контейнера: \n"
+              << "1) Ввод с клавиатуры.\n"
+              << "2) Ввод с помощью рандомайзера.\n"
+              << "3) Ввод данных из текстового файла.\n";
     std::cin >> method;
 
     switch (method) {
     case 1: {
         for (int i = 0; i < size; ++i) {
             int value = 0;
-            std::cout << "Ââåäèòå ýëåìåíò " << i + 1 << ": ";
+            std::cout << "Введите элемент " << i + 1 << ": ";
             if (!(std::cin >> value)) {
-                std::cerr << "Îøèáêà ââîäà äàííûõ\n";
+                std::cerr << "Ошибка ввода данных\n";
                 return false;
             }
             container.insert(container.end(), value);
@@ -37,13 +37,13 @@ void FillContainer(Container& container, int size) {
     case 3: {
         std::ifstream inputFile("input.txt");
         if (!inputFile) {
-            std::cerr << "Îøèáêà: Íå óäàëîñü îòêðûòü ôàéë\n";
+            std::cerr << "Ошибка: Не удалось открыть файл\n";
             return false;
         }
         for (int i = 0; i < size; ++i) {
             int value = 0;
             if (!(inputFile >> value)) {
-                std::cerr << "Îøèáêà: Íåäîñòàòî÷íî äàííûõ â ôàéëå\n";
+                std::cerr << "Ошибка: Недостаточно данных в файле\n";
                 return false;
             }
             container.insert(container.end(), value);
@@ -51,34 +51,40 @@ void FillContainer(Container& container, int size) {
         break;
     }
     default:
-        std::cerr << "Îøèáêà: Íåâåðíûé ìåòîä ââîäà\n";
+        std::cerr << "Ошибка: Неверный метод ввода\n";
         return false;
     }
     return true;
 }
 
 template <typename Container>
-void PrintContainer(const Container& container) {
-    std::cout << "Ýëåìåíòû (ïðÿìîé ïîðÿäîê): ";
+void ContainerOperations::PrintContainer(const Container& container) {
+    std::cout << "Элементы (прямой порядок): ";
+    
+    // Итератор для прямого прохода (ожидаемый тип: Container::const_iterator)
     for (auto it = container.begin(); it != container.end(); ++it) {
         std::cout << *it << " ";
     }
     std::cout << "\n";
 
-    std::cout << "Ýëåìåíòû (îáðàòíûé ïîðÿäîê): ";
+    std::cout << "Элементы (обратный порядок): ";
+    
+    // Обратный итератор (ожидаемый тип: Container::const_reverse_iterator)
     for (auto rit = container.rbegin(); rit != container.rend(); ++rit) {
         std::cout << *rit << " ";
     }
     std::cout << "\n";
 }
 
-void InsertElements(std::deque<int>& sourceDeque, std::list<int>& targetList) {
+void ContainerOperations::InsertElements(std::deque<int>& sourceDeque, std::list<int>& targetList) {
+    // Итератор на элемент списка ( ожидаемый тип: std::list<int>::iterator)
     auto insertPosition = targetList.end();
     std::advance(insertPosition, -5);
     targetList.insert(insertPosition, sourceDeque.rbegin(), sourceDeque.rbegin() + 5);
 }
 
-template void FillContainer(std::deque<int>&, int);
-template void FillContainer(std::list<int>&, int);
-template void PrintContainer(const std::deque<int>&);
-template void PrintContainer(const std::list<int>&);
+// Явная инстанциация шаблонов
+template bool ContainerOperations::FillContainer(std::deque<int>&, int);
+template bool ContainerOperations::FillContainer(std::list<int>&, int);
+template void ContainerOperations::PrintContainer(const std::deque<int>&);
+template void ContainerOperations::PrintContainer(const std::list<int>&);
